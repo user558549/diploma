4106a155436e883963123fc8ecf9f1b164aa0000
Konstantin <kostya.tarhanov@mail.ru>
2026-07-24 23:57:34 +0500

    fix URL in RoomPage

--- src/pages/room/RoomPage.jsx
+++ src/pages/room/RoomPage.jsx
@@ -22,7 +22,7 @@ export const RoomPage = () => {
     serverData: oneRoom,
     isLoading,
     error,
-  } = useFetch(`http://localhost:3001/rooms/${params.number_room}`);
+  } = useFetch(`http://94.102.89.76:5000/rooms/${params.number_room}`);
 
   return isLoading ? (
     <Loader />
